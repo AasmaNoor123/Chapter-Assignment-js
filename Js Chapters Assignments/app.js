8be60b8a007a30b774709aa56648e99475e77d21
Aasma Noor <aasmanoor511@gmail.com>
2024-10-28 18:28:16 +0500

    Add files via upload

---
+++ Js Chapters Assignments/app.js
@@ -0,0 +1,30 @@
+//Variables of Numbers:
+//Assignment : 3 (Chapter : 3)
+//Q : 1
+
+// let age = 17;
+// alert('I am '+ age + ' old');
+
+// //Q : 2
+
+// let data = prompt(' How many time you have visited this side ');
+// document.write('you have visited ' +  data +" time this side");
+
+// //Q : 3
+// //(typeof) kisi bhi cheez ki data type check kr ky bta deta hai ky (number )hai ya (string).
+
+// let birth = 2007;
+// document.write('My birth year is '+ birth +'<br>');
+// document.write('Data type of my declared variable is  '+ typeof(birth));
+
+//Q : 4
+let visitor = prompt('Hello Visitor, Enter your name... ');
+let product = prompt('What do you want to buy...');
+let quantity = prompt('How many?');
+
+document.write("<b>"+visitor+"</b>", ' ordered ', '<b>'+ quantity + '</b>', "  ",   "<b>" + product +"</b>"," (s) " + ' on xyz clothing store ' );
+
+
+
+
+
