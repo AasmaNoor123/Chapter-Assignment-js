f5eda3d5f05fd92547d53857ac893949ef791c39
Aasma Noor <aasmanoor511@gmail.com>
2024-10-30 10:14:05 +0500

    Update app.js

--- Js Chapters Assignments/app.js
+++ Js Chapters Assignments/app.js
@@ -1,3 +1,68 @@
+//Assignment : 2
+//Chapter : 2 (Vaiables for strings)
+
+/*1. Declare a variable called username.*/
+
+let username;
+
+//2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+let name = 'Aasma Noor';
+
+//Question : 3
+let  name2 = alert('Johne Doe...')
+
+//4. Write script to
+
+//a) Declare a JS variable, titled message.
+
+//b) Assign “Hello World” to variable message
+//c) Display the message in alert box.
+
+//Ex : 1
+let message =alert('Hello World 🌎');
+
+//Ex : 1
+//let name = 'Jhone Doe .';
+
+//alert('Hey, My name is : '+name);
+
+
+//Ex : 2
+
+let age = alert("15 years old...");
+
+// Ex : 3;
+let course = alert("Certified Mobile Application Development");
+
+//let age = '15 years old .';
+
+//alert('I am  :  '+age); 
+
+//5. Write a script to display the following alert using one JS 
+
+let pizza = alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+//6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+
+let email = 'example@example.com.';
+
+alert('My email address is :  '+email); 
+
+//7. Declare a variable called book & give it the value “A
+//smarter way to learn JavaScript”Display the following
+//message in an alert box:
+
+let book = alert('A Smarter way to learn jacascript.');
+
+//8. Write a script to display this in browser through JS
+
+document.write("Yah! I can write HTML content throught JavaScript...✨");
+
+//9. Store following string in a variable and show in alert and browser through JS“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”-- 
+
+let string = alert("---------ஜ۩۞۩ஜ---------");
+
 //Variables of Numbers:
 //Assignment : 3 (Chapter : 3)
 //Q : 1
